781af93c7c32bc5952e00d6d2895e9b3ece9e18c
João Paulo <joaopaulo.data@gmail.com>
2022-09-06 13:53:32 -0300

    docs: 📑  adicionando props

--- CursoVue2/main.js
+++ CursoVue2/main.js
@@ -1,5 +1,5 @@
 Vue.component('message', {
-    // props: ['title', 'body'],
+    props: ['title', 'body'],
   
     data() {
       return {
@@ -10,15 +10,11 @@ Vue.component('message', {
     template: `
         <article class="message" v-show="visible">
             <div class="message-header">
-                <p>Olá mundo, Código refinado!</p>
+                <p>{{ title }}</p>
                 <button class="delete" @click="hideMessage"></button>
             </div>
             <div class="message-body">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis
-                placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum
-                <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et
-                sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem
-                eget, facilisis sodales sem.
+                {{ body }}
             </div>
         </article>
       `,
