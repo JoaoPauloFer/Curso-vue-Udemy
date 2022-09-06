b1e87983c6f081e2d13ecbd7781c193b2f9bc2db
João Paulo <joaopaulo.data@gmail.com>
2022-09-06 13:45:29 -0300

    docs: 📑  criando componentes e adicionando o bulma

--- CursoVue2/main.js
+++ CursoVue2/main.js
@@ -1,23 +1,38 @@
-Vue.component('lista-tarefas', {
-
-    template: '<div><tarefa v-for="tarefa in tarefas"> {{ tarefa.descricao }} </tarefa></div>',
-
+Vue.component('message', {
+    // props: ['title', 'body'],
+  
     data() {
-        return {
-            tarefas:[
-                { descricao: 'Ir ao Banco no Brasil' },
-                { descricao: 'Ir gravar as aulas' },
-                { descricao: 'Ir para a aula de inglês' }
-            ]
-        }
+      return {
+        visible: true
+      };
+    },
+  
+    template: `
+        <article class="message" v-show="visible">
+            <div class="message-header">
+                <p>Olá mundo, Código refinado!</p>
+                <button class="delete" @click="hideMessage"></button>
+            </div>
+            <div class="message-body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis
+                placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum
+                <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et
+                sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem
+                eget, facilisis sodales sem.
+            </div>
+        </article>
+      `,
+  
+    methods: {
+      hideMessage(){
+        this.visible = false;
+      }
     }
-})
-
-
-Vue.component('tarefa', {
-    template: '<li> <slot></slot> </li>',
-})
-
-new Vue({
+  
+  
+  });
+  
+  
+  new Vue({
     el: '#root'
-})
+  });
