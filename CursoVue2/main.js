683d88782de1e6793372df70791ab0a8abacfcb1
João Paulo <joaopaulo.data@gmail.com>
2022-09-08 09:29:48 -0300

    docs: 📑  Criando modal

--- CursoVue2/main.js
+++ CursoVue2/main.js
@@ -1,34 +1,22 @@
-Vue.component('message', {
-    props: ['title', 'body'],
-  
-    data() {
-      return {
-        visible: true
-      };
-    },
-  
-    template: `
-        <article class="message" v-show="visible">
-            <div class="message-header">
-                <p>{{ title }}</p>
-                <button class="delete" @click="hideMessage"></button>
+Vue.component('modal', {
+  template: `
+          <div class="modal is-active">
+            <div class="modal-background"></div>
+            <div class="modal-content">
+              <div class="box">
+                <slot></slot>
+              </div>
             </div>
-            <div class="message-body">
-                {{ body }}
-            </div>
-        </article>
-      `,
-  
-    methods: {
-      hideMessage(){
-        this.visible = false;
-      }
-    }
-  
-  
-  });
-  
-  
-  new Vue({
-    el: '#root'
-  });
+            <button class="modal-close" @click="$emit('meuclickfechar')"></button>
+          </div>
+    `
+
+});
+
+
+new Vue({
+  el: '#root',
+  data: {
+    showModal: false
+  }
+});
