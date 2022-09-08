84797ed6204c94cf14a60cda7614ffefa75b0257
João Paulo <joaopaulo.data@gmail.com>
2022-09-08 11:28:43 -0300

    docs: 📑  Utilizando slots nomeados

--- CursoVue2/main.js
+++ CursoVue2/main.js
@@ -1,14 +1,31 @@
 Vue.component('modal', {
   template: `
-          <div class="modal is-active">
-            <div class="modal-background"></div>
-            <div class="modal-content">
-              <div class="box">
-                <slot></slot>
-              </div>
-            </div>
-            <button class="modal-close" @click="$emit('meuclickfechar')"></button>
+        <div class="modal is-active">
+          <div class="modal-background"></div>
+          <div class="modal-card">
+          
+          
+            <header class="modal-card-head">
+              <p class="modal-card-title">
+                <slot name="header"></slot>
+               </p>
+              
+              <button class="delete" @click="$emit('meuclickfechar')"></button>
+              
+            </header>
+            
+            
+            <section class="modal-card-body">
+              <slot>Se eu escreve algo na tag slot padrão</slot>
+            </section>
+            
+            <footer class="modal-card-foot">
+              
+              <slot name="footer"> <b>Footer</b> </slot>
+
+            </footer>
           </div>
+        </div>
     `
 
 });
