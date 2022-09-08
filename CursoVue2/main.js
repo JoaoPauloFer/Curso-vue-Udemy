a7a160243075a15acbf2a097b6519246622cfa40
João Paulo <joaopaulo.data@gmail.com>
2022-09-08 12:14:59 -0300

    docs: 📑  Trabalhando com inline template

--- CursoVue2/main.js
+++ CursoVue2/main.js
@@ -1,67 +1,11 @@
-Vue.component('tabs', {
-
-  template: `
-
-   <div>
-    <div class="tabs">
-      <ul>
-      
-        <li v-for="tab in tabs" :class="{'is-active': tab.isActive}"> 
-         <a href="#" @click="selectTab(tab)">{{ tab.name}}</a> 
-        </li>
-        
-      </ul>
-    </div>
-    
-    <div class="tabs-detail">
-      <slot></slot>
-    </div>
-    
-  </div>
-  `,
+Vue.component('progresso', {
 
   data() {
-    return {
-      tabs: []
-    };
-  },
-
-  created() {
-    this.tabs = this.$children;
-  },
-
-  methods: {
-    selectTab(selectedTab) {
-      this.tabs.forEach(tab => {
-        tab.isActive = (tab.name == selectedTab.name);
-      });
-    }
+    return {porcentagemProgresso: 10};
   }
 
 });
 
-Vue.component('tab', {
-
-  props: {
-    name: {required: true},
-    selected: {default: false}
-  },
-
-  data(){
-    return {
-       isActive:  false
-    };
-  },
-
-  mounted() {
-    this.isActive = this.selected;
-  },
-
-  template: `
-    <div v-show="isActive"><slot></slot></div>
-  `
-
-});
 
 new Vue({
   el: '#root'
